10923738a5ed5a51abc007152aee7680a61db8ea
yegeunyang <yegeunyang@icloud.com>
2025-01-08 10:00:07 -0600

    Add grid items using JavaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+const gridContainer = document.querySelector('.grid-container');
+
+for (let i = 0; i < 16 * 16; i++) {
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('grid-item');
+    gridContainer.appendChild(gridItem);
+}
