d2b1c7ae4fdd091411538c8525155da265cbc1a5
yegeunyang <yegeunyang@icloud.com>
2025-01-08 12:28:42 -0600

    Add hovering effect to the grid

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const gridContainer = document.querySelector('.grid-container');
+const gridContainer = document.querySelector(".grid-container");
 
-for (let i = 0; i < 16 * 16; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    gridContainer.appendChild(gridItem);
+for (let i = 0; i < 16; i++) {
+    const gridItemColumn = document.createElement("div");
+    gridItemColumn.classList.add("grid-item-column");
+    gridContainer.appendChild(gridItemColumn);
+
+    for (let j = 0; j < 16; j++) {
+        const gridItemRow = document.createElement("div");
+        gridItemRow.classList.add("grid-item-row");
+        gridItemColumn.appendChild(gridItemRow);
+    }
 }
+
+let gridItemRow = document.querySelectorAll(".grid-item-row");
+gridItemRow.forEach((gridItemRow) => {
+    gridItemRow.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = "white";
+    });
+});
